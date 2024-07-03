5619c6e185901868c15db52792c77752b922377e
pallavideepa <pallavilj.xworkz@gmail.com>
2024-07-03 11:09:50 +0530

    hhhhhh

---
+++ Stationary.java
@@ -0,0 +1,13 @@
+class Stationary{
+	static String stationaryName = "Sai Books Store";
+	static String itemMenus[] = {"Marker", "Rubber Stamp" , "Eraser" , "PushPin" , "BallPointPen" , "Sticky Notes" , "EnvelopeCover" , "Highlighter" , "Clipboard" , "PaperClips" , "Scissors" , "Bulldog Clip" , "PencilSharpener" , "Calculator" , "Stapler" , "Pencils" , "Scale" , "Fevicol" , "Sticker" , "InkPot" , "Geometry Box"};
+	public static void main(String[] items){
+		System.out.println("Main Started");
+			for(String itemMenu : itemMenus){
+				System.out.println(itemMenu);
+			}
+				//System.out.println("Stationary Menu list are :" +itemMenus[0]+ " \n " +itemMenus[1]+ " \n " +itemMenus[2]+ " \n " +itemMenus[3]+ " \n " +itemMenus[4]+ " \n " +itemMenus[5]+ " \n " +itemMenus[6]+ " \n " +itemMenus[7]+ " \n " +itemMenus[8]+ " \n " +itemMenus[9]+ " \n " +itemMenus[10]+ " \n " +itemMenus[11]+ " \n " +itemMenus[12]+ " \n " +itemMenus[13]+ " \n " +itemMenus[14]+ " \n " +itemMenus[15]+ " \n " +itemMenus[16]+ " \n " +itemMenus[17]+ " \n " +itemMenus[18]+ " \n " +itemMenus[19]);
+		System.out.println("Main Ended");
+		
+	}
+}
